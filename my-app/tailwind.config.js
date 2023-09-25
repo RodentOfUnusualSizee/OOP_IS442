f5f4e2c53c61f0f17525426151de86823ef1d54b
yyyanwee <89122919+yyyanwee@users.noreply.github.com>
2023-09-25 23:04:49 +0800

    added tailwindcss to gitignore, created gscolours in config

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -9,8 +9,23 @@ module.exports = {
       {
         colors: {
           'gsblue': '#749AC7',
-          'gswhite': '#FFFFFF',
-          'gsgray90': '#1C2B36'
+          'gswhite': '#FFFFFF', 
+          'gsblue40': '#75B1FF', 
+          'gsblue50': '#3D8DF5',
+          'gsblue60': '#186ADE', //For Primary Calls to Action, Important Information
+          'gsblue70': '#0D4EA6',
+          'gsblue80': '#103A75',
+          'gsgray20': '#DCE3E8', // Tertiary Surface/BG, Minimal Borders
+          'gsgray40': '#9FB1BD', //For Tertiary Text, Secondary Icons
+          'gsgray70': '#3E5463', //For Secondary Text, Primary Icons
+          'gsgray90': '#1C2B36', //For Primary Text, High Contrast Icons
+          'gsgreen40': '#43C478',
+          'gsgreen50': '#16A163',
+          'gsgreen60': '#077D55', //For Positive Values, Success, Available, Confirmed
+          'gsgreen70': '#075E45',
+          'gsyellow30': '#F5C518', //For warning, mild alerts
+          'gsred60': '#D91F11', //Critical Alert/ Errors, Negative Values
+          'gsorange50': '#E86427', //For Warning, Mild Alerts
         },
       }
     ),
