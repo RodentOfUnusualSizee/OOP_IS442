0eccc57ec545fae1492b795641cfbac190390eed
Brandonchriss <bkretser.2020@sis.smu.edu.sg>
2023-09-25 14:07:43 +0800

    config change

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts}"],
+  content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      'gs-blue': '#749AC7',
+      'gs-white': '#FFFFFF'
+    }
   },
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   plugins: [],
 }
 
