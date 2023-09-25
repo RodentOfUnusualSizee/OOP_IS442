647c9d897893873df3da034a2cac9419e274ea8c
Brandonchriss <bkretser.2020@sis.smu.edu.sg>
2023-09-25 15:12:58 +0800

    config change

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,7 @@ import { fetchData, postData } from './api/APIUtilsTemplate'; // Adjust the path
 import Header from './components/Header';
 import logo from './logo.svg';
 import './App.css';
+import './tailwind.css';
 
 function App() {
   // SAMPLE POST AND GET REQUEST USED IN USEEFFECT CAN REMOVE AFTER U UNDERSTAND
@@ -34,6 +35,14 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+        <p className="text-3xl text-gray-700 font-bold mb-5">
+          Welcome!
+        </p>
+        <p className="text-gray-500 text-sm">
+          React and Tailwind CSS in action
+        </p>
+      </div>
     </div>
   );
 }
--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../tailwind.css';
 
 
 const Header = () => {
+    const userLinks = [
+        {
+            name: "Portfolio",
+            link: "/portfolio"
+        },
+        {
+            name: "Stocks",
+            link: "/stocks"
+        },
+        {
+            name: "Invest",
+            link: "/invest"
+        }
+    ];
+
+    const adminLinks = [
+        {
+            name: "User Management",
+            link: "/user-management"
+        },
+        {
+            name: "Portfolio Watch",
+            link: "/portfolio-watch"
+        },
+        {
+            name: "Data Management",
+            link: "/data-management"
+
+        }
+    ];
+
+    const [user, getUser] = useState<String>("user");
+    const [isLogin, getLogin] = useState<Boolean>(false);
+
+
     return (
-        <header className="bg-white">
+        <header className="bg-blue">
             <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
                 <a className="block text-t  eal-600" href="/">
                     <span className="sr-only">Home</span>
@@ -12,18 +47,41 @@ const Header = () => {
 
                 <div className="flex flex-1 items-center justify-end md:justify-between">
                     <nav aria-label="Global" className="hidden md:block">
-                        <ul className="flex items-center gap-6 text-sm">
-                            <li>
-                                <a className="text-gray-500 transition hover:text-gray-500/75" href="/">Link</a>
-                            </li>
-                        </ul>
+                        {user === "user" ? (
+                            <ul className="flex items-center gap-6 text-sm">
+                                {userLinks.map(({ name, link }) => (
+                                    <li key={link}>
+                                        <a className="text-gray-500 transition hover:text-gray-500/75" href="/">
+                                            {name}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <ul className="flex items-center gap-6 text-sm">
+                                {adminLinks.map(({ name, link }) => (
+                                    <li key={link}>
+                                        <a className="text-gray-500 transition hover:text-gray-500/75" href="/">
+                                            {name}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        )
+                        }
                     </nav>
 
                     <div className="flex items-center gap-4">
-                        <div className="sm:flex sm:gap-4">
-                            <a className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700" href="/">Login</a>
-                            <a className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block" href="/">Register</a>
-                        </div>
+                        {!isLogin ? (
+                            <div className="sm:flex sm:gap-4">
+                                <a className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700" href="/">Login</a>
+                                <a className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block" href="/">Register</a>
+                            </div>
+                        ) : (
+                            <div className="sm:flex sm:gap-4">
+                                <a className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block" href="/">Log Out</a>
+                            </div>
+                        )}
 
                         <button className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
                             <span className="sr-only">Toggle menu</span>
