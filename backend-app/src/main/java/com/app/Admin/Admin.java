08b54e17fbe17ea3bf2cde9241baf53dbc0fecb5
MrTaooo <95959934+MrTaooo@users.noreply.github.com>
2023-10-09 23:33:45 +0800

    Commented Admin.java

--- backend-app/src/main/java/com/app/Admin/Admin.java
+++ backend-app/src/main/java/com/app/Admin/Admin.java
@@ -1,27 +1,27 @@
-package com.app.Admin;
-import com.app.User.User;
-import org.springframework.stereotype.Service;
+// package com.app.Admin;
+// import com.app.User.User;
+// import org.springframework.stereotype.Service;
 
 
-@Service
-public class Admin extends User {
-    public Admin(String email, String password, String firstName, String lastName, String role) {
-        super(email, password, firstName, lastName, role);
-    };
+// @Service
+// public class Admin extends User {
+//     public Admin(String email, String password, String firstName, String lastName, String role) {
+//         super(email, password, firstName, lastName, role);
+//     };
 
-    public User createUser(String email, String password, String firstName, String lastName, String role) {
-        return new User(email, password, firstName, lastName, role);
-    }
+//     public User createUser(String email, String password, String firstName, String lastName, String role) {
+//         return new User(email, password, firstName, lastName, role);
+//     }
 
-    public User editUser(String email, String password, String firstName, String lastName, String role) {
-        return new User(email, password, firstName, lastName, role);
-    }
+//     public User editUser(String email, String password, String firstName, String lastName, String role) {
+//         return new User(email, password, firstName, lastName, role);
+//     }
 
-    public boolean deleteUser() {
-        return true;
-    }
+//     public boolean deleteUser() {
+//         return true;
+//     }
 
-    public User viewUser(String email, String password, String firstName, String lastName, String role) {
-        return new User(email, password, firstName, lastName, role);
-    }
-}
+//     public User viewUser(String email, String password, String firstName, String lastName, String role) {
+//         return new User(email, password, firstName, lastName, role);
+//     }
+// }
