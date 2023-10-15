81c3e50014a9960dc6a7cf02c22a4c08824fdc0b
Lam Ching Rou <crlam.2020@smu.edu.sg>
2023-10-15 16:21:52 +0800

    add cors

--- backend-app/src/main/java/com/app/Portfolio/PortfolioController.java
+++ backend-app/src/main/java/com/app/Portfolio/PortfolioController.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/portfolio")
 public class PortfolioController {
--- backend-app/src/main/java/com/app/User/UserController.java
+++ backend-app/src/main/java/com/app/User/UserController.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
