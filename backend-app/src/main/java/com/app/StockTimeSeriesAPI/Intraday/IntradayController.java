be326c81de47674c25d8d826b523a675ddd06521
MrTaooo <lintao199@gmail.com>
2023-10-15 20:40:06 +0800

    Stock Series Intraday

---
+++ backend-app/src/main/java/com/app/StockTimeSeriesAPI/Intraday/IntradayController.java
@@ -0,0 +1,98 @@
+package com.app.StockTimeSeriesAPI.Intraday;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import com.app.StockTimeSeriesAPI.Intraday.StockTimeSeriesIntradayDTO.IntradayStockData;
+import com.app.StockTimeSeriesAPI.Intraday.StockTimeSeriesIntradayDTO.MetaData;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/stock/intradayTimeSeries")
+public class IntradayController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private String apiKey;
+
+    @GetMapping("/{symbol}/{interval}")
+    public StockTimeSeriesIntradayDTO getIntradayTimeSeries(@PathVariable String symbol, @PathVariable String interval) {
+
+        Dotenv dotenv = Dotenv.load();
+        this.apiKey = dotenv.get("ALPHAVANTAGE_APIKEY");
+
+        String apiUrl = String.format(
+                "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&interval=%smin&apikey=%s",
+                symbol, interval, apiKey);
+
+        ResponseEntity<Map> response = restTemplate.getForEntity(apiUrl, Map.class);
+
+        Map<String, Object> responseBody = response.getBody();
+
+        // Implement your mapping logic to map the response to your StockTimeSeriesIntradayDTO.
+        // Note: Normally, you would want to create a service layer to handle the
+        // business logic
+        // and keep your controller clean.
+        StockTimeSeriesIntradayDTO StockTimeSeriesIntradayDTO = mapResponseToDTO(responseBody,interval);
+
+        // Pass StockTimeSeriesIntradayDTO to your internal system here
+
+        return StockTimeSeriesIntradayDTO;
+        // return responseBody;
+    }
+
+    private StockTimeSeriesIntradayDTO mapResponseToDTO(Map<String, Object> responseBody,String interval) {
+        StockTimeSeriesIntradayDTO StockTimeSeriesIntradayDTO = new StockTimeSeriesIntradayDTO();
+        MetaData metaData = new MetaData();
+        Map<String, IntradayStockData> timeSeriesIntraday = new HashMap<>();
+
+        // Parsing MetaData
+        Map<String, String> apiMetaData = (Map<String, String>) responseBody.get("Meta Data");
+        metaData.setInformation(apiMetaData.get("1. Information"));
+        metaData.setSymbol(apiMetaData.get("2. Symbol"));
+        metaData.setLastRefreshed(apiMetaData.get("3. Last Refreshed"));
+        metaData.setInterval(apiMetaData.get("4. Interval"));
+        metaData.setOutputSize(apiMetaData.get("5. Output Size"));
+        metaData.setTimeZone(apiMetaData.get("6. Time Zone"));
+        StockTimeSeriesIntradayDTO.setMetaData(metaData);
+
+        // Parsing Time Series (Intraday)
+        String getString = String.format("Time Series (%smin)", interval);
+        System.out.println("FUCKER START");
+        System.out.println(interval);
+        System.out.println(getString);
+        System.out.println("FUCKER END");
+        Map<String, Map<String, String>> apiTimeSeriesIntraday = (Map<String, Map<String, String>>) responseBody
+                .get(getString);
+
+        // Date format in API response is: "yyyy-MM-dd"
+        // SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        for (Map.Entry<String, Map<String, String>> entry : apiTimeSeriesIntraday.entrySet()) {
+            IntradayStockData intradayStockData = new IntradayStockData();
+            intradayStockData.setOpen(Double.parseDouble(entry.getValue().get("1. open")));
+            intradayStockData.setHigh(Double.parseDouble(entry.getValue().get("2. high")));
+            intradayStockData.setLow(Double.parseDouble(entry.getValue().get("3. low")));
+            intradayStockData.setClose(Double.parseDouble(entry.getValue().get("4. close")));
+            intradayStockData.setVolume(Long.parseLong(entry.getValue().get("5. volume")));
+
+            try {
+                String dateKey = entry.getKey();
+                timeSeriesIntraday.put(dateKey, intradayStockData);
+            } catch (Exception e) {
+                e.printStackTrace(); // Consider better exception handling
+            }
+        }
+        StockTimeSeriesIntradayDTO.setTimeSeries(timeSeriesIntraday);
+
+        return StockTimeSeriesIntradayDTO;
+    }
+}
---
+++ backend-app/src/main/java/com/app/StockTimeSeriesAPI/Intraday/StockTimeSeriesIntradayDTO.java
@@ -0,0 +1,118 @@
+package com.app.StockTimeSeriesAPI.Intraday;
+import java.util.*;
+
+// Stock Time Series Data Transfer Object
+public class StockTimeSeriesIntradayDTO {
+
+    private MetaData metaData;
+    private Map<String, IntradayStockData> intradayTimeSeries;
+
+    // Getters
+    public MetaData getMetaData() {
+        return metaData;
+    }
+    public Map<String, IntradayStockData> getTimeSeries() {
+        return intradayTimeSeries;
+    }
+
+    // Setters
+    public void setMetaData(MetaData metaData) {
+        this.metaData = metaData;
+    }
+    public void setTimeSeries(Map<String, IntradayStockData> intradayTimeSeries) {
+        this.intradayTimeSeries = intradayTimeSeries;
+    }
+
+    // Inner class for Meta Data
+    public static class MetaData {
+        private String information;
+        private String symbol;
+        private String lastRefreshed;
+        private String interval; // 1,5,15,30,60
+        private String outputSize;
+        private String timeZone;
+
+        // Getters
+        public String getInformation() {
+            return information;
+        }
+        public String getSymbol() {
+            return symbol;
+        }
+        public String getLastRefreshed() {
+            return lastRefreshed;
+        }
+        public String getInterval() {
+            return interval;
+        }
+        public String getOutputSize() {
+            return outputSize;
+        }
+        public String getTimeZone() {
+            return timeZone;
+        }
+
+        // Setters
+        public void setInformation(String information) {
+            this.information = information;
+        }
+        public void setSymbol(String symbol) {
+            this.symbol = symbol;
+        }
+        public void setLastRefreshed(String lastRefreshed) {
+            this.lastRefreshed = lastRefreshed;
+        }
+        public void setInterval(String interval) {
+            this.interval = interval;
+        }
+        public void setOutputSize(String outputSize) {
+            this.outputSize = outputSize;
+        }
+        public void setTimeZone(String timeZone) {
+            this.timeZone = timeZone;
+        }
+    }
+
+    // Inner class for Intraday Stock Data
+    public static class IntradayStockData {
+        private double open;
+        private double high;
+        private double low;
+        private double close;
+        private long volume;
+
+        // Getters
+        public double getOpen() {
+            return open;
+        }
+        public double getHigh() {
+            return high;
+        }
+        public double getLow() {
+            return low;
+        }
+        public double getClose() {
+            return close;
+        }
+        public long getVolume() {
+            return volume;
+        }
+
+        // Setters
+        public void setOpen(double open) {
+            this.open = open;
+        }
+        public void setHigh(double high) {
+            this.high = high;
+        }
+        public void setLow(double low) {
+            this.low = low;
+        }
+        public void setClose(double close) {
+            this.close = close;
+        }
+        public void setVolume(long volume) {
+            this.volume = volume;
+        }
+    }
+}
