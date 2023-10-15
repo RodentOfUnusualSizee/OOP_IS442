9f8afbd26a3983995cdae009a1d7ec015dd09dae
Brandonchriss <bkretser.2020@sis.smu.edu.sg>
2023-10-16 02:28:19 +0800

    ez fix table

--- my-app/src/components/Table.tsx
+++ my-app/src/components/Table.tsx
@@ -11,52 +11,50 @@ interface TableProps {
 const Table = ({ tableData, tableHeaders, tableTitle, tableDescription, tableAction }: TableProps) => {
     return (
         <div>
-            <h1 className="text-base font-semibold leading-6 text-gray-900">
-                {tableTitle}
-            </h1>
-            <div>
-                <div className="sm:flex sm:items-center">
-                    <div className="sm:flex-auto">
-                        <p className="mt-2 text-sm text-gray-700">{tableDescription}</p>
-                    </div>
-                    <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                        {tableAction}
-                    </div>
+            <div className="sm:flex sm:items-center">
+                <h1 className="text-base font-semibold leading-6 text-gray-900">
+                    {tableTitle}
+                </h1>
+                <div className="sm:flex-auto">
+                    <p className="mt-2 text-sm text-gray-700">{tableDescription}</p>
                 </div>
-                <div className="flex flex-col mt-4">
-                    <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                                <table className="min-w-full divide-y divide-gray-200">
-                                    <thead className="bg-gray-50">
-                                        <tr>
+                <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                    {tableAction}
+                </div>
+            </div>
+            <div className="flex flex-col mt-4">
+                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table className="min-w-full divide-y divide-gray-200">
+                                <thead className="bg-gray-50">
+                                    <tr>
+                                        {tableHeaders.map((header) => (
+                                            <th
+                                                key={header.key}
+                                                scope="col"
+                                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >
+                                                {header.header}
+                                            </th>
+                                        ))}
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-gray-200">
+                                    {tableData.map((row) => (
+                                        <tr key={row.id}>
                                             {tableHeaders.map((header) => (
-                                                <th
+                                                <td
                                                     key={header.key}
-                                                    scope="col"
-                                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                                    className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
                                                 >
-                                                    {header.header}
-                                                </th>
+                                                    {row[header.key]}
+                                                </td>
                                             ))}
                                         </tr>
-                                    </thead>
-                                    <tbody className="bg-white divide-y divide-gray-200">
-                                        {tableData.map((row) => (
-                                            <tr key={row.id}>
-                                                {tableHeaders.map((header) => (
-                                                    <td
-                                                        key={header.key}
-                                                        className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
-                                                    >
-                                                        {row[header.key]}
-                                                    </td>
-                                                ))}
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
+                                    ))}
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
