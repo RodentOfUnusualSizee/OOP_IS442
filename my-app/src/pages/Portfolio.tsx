4703000fc7822c09c00ca4647c23bd2211000a64
Lam Ching Rou <crlam.2020@smu.edu.sg>
2023-10-23 16:12:08 +0800

    resolve merge conflict

--- my-app/src/pages/Portfolio.tsx
+++ my-app/src/pages/Portfolio.tsx
@@ -37,14 +37,13 @@ function Portfolio() {
         setQuantity("");
         setPrice("");
     }
-
+    
     // form inputs
     const [side, setSide] = React.useState<string>("");
     const [stockCode, setStockCode] = React.useState<string>("");
     const [date, setDate] = React.useState<string>("");
     const [quantity, setQuantity] = React.useState<string>("");
     const [price, setPrice] = React.useState<string>("");
-    const [data, setData] = React.useState<[]>([]);
     
     // get today's date and parse as string containing yy/mm/dd
     const today = new Date();
@@ -73,7 +72,7 @@ function Portfolio() {
             console.log(error);
         });
     }
-
+    
     const summary = () => {
         const summary = document.getElementById("summary") as HTMLSpanElement;
         summary.innerHTML = side + " " + quantity + " " + stockCode + " @ $" + price + " on " + date;
@@ -148,54 +147,53 @@ function Portfolio() {
 
     for (let i = 0; i < data.length; i++) {
         // get specific portfolio
-        if (data[i].portfolioID == portfolioId){
+        if (data[i].portfolioID.toString() == portfolioId){
             portfolio = data[i];
 
-    //         // portfolio metrics
-    //         samplePortfolioData = { id: portfolio.portfolioID, name: portfolio.portfolioName, strategy: portfolio.strategyDesc, capital: portfolio.capitalUSD };
+            // portfolio metrics
+            samplePortfolioData =  { id: portfolio.portfolioID, name: portfolio.portfolioName, strategy: portfolio.strategyDesc, capital: portfolio.capitalUSD};
 
-    //         // table
-    //         stockTableData = portfolio.cumPositions;
+            // table
+            stockTableData = portfolio.cumPositions;
 
-    //         // parse through portfolio data
-    //         let allocation = portfolio.portfolioAllocationBySector;
-    //         let historicalVal = portfolio.portfolioHistoricalValue;
-    //         let positions = portfolio.positions;
+            // parse through portfolio data
+            let allocation = portfolio.portfolioAllocationBySector;
+            let historicalVal = portfolio.portfolioHistoricalValue;
+            let positions = portfolio.positions;
 
-    //         for (var k in allocation) {
-    //             let val = roundTo(allocation[k], 4);
-    //             piechartdata.push({ "name": k, "value": val });
-    //         }
+            for (var k in allocation){
+                let val = roundTo(allocation[k], 4);
+                piechartdata.push({"name": k, "value":  val});
+            }
 
-            let firstVal = ""; // first historical price value
-            let lastVal = ""; // last historical price value
+            let firstVal = 0; // first historical price value
+            let lastVal = 0; // last historical price value
 
-    //         // line chart
-    //         for (var h in historicalVal) {
-    //             linechartdata.push({ "date": h, "price": historicalVal[h] })
-    //             if (!lastVal) {
-    //                 lastVal = historicalVal[h];
-    //             }
-    //             firstVal = historicalVal[h];
-    //         }
+            // line chart
+            for (var h in historicalVal){
+                linechartdata.push({"date": h, "price": historicalVal[h]})
+                if (!lastVal){
+                    lastVal = historicalVal[h];
+                }
+                firstVal = historicalVal[h];
+            }
 
             // capital change metrics
-            let diffAbs = parseFloat(lastVal) - parseFloat(firstVal);
-            let diffPercent = ((parseFloat(lastVal) - parseFloat(firstVal)) / parseFloat(lastVal)) * 100
+            let diffAbs = lastVal - firstVal;
+            let diffPercent = ((lastVal - firstVal) / lastVal) * 100
 
-    //         diffAbs = roundTo(diffAbs, 2)
-    //         diffPercent = roundTo(diffPercent, 2)
+            diffAbs = roundTo(diffAbs, 2)
+            diffPercent = roundTo(diffPercent, 2)
 
             if (diffAbs > 0){
                 capitalChangeAbs = "+$" + diffAbs;
                 capitalChangePercent = diffPercent + "%";
             } else {
-                capitalChangeAbs = "-$" + diffAbs;
+                capitalChangeAbs = "-$" + Math.abs(diffAbs);
                 capitalChangePercent = diffPercent + "%";
             }
 
             // last modified date metric
-
             let modified = new Date("1998-01-01T00:00:00.000+00:00"); // initialise to 1st Jan 1998
 
             for (let p = 0; p < positions.length; p++) {
@@ -206,25 +204,23 @@ function Portfolio() {
                 }
             }
             
-            let currDate = new Date();
-            let timeDiff = currDate.getTime() - modified.getTime();
+            let timeDiff = today.getTime() - modified.getTime();
             lastModified = roundTo(timeDiff / (1000 * 60 * 60 * 24), 0) + " days";
-
         }
     }
 
     const stats = [
         { name: 'Capital Change ($) ', stat: capitalChangeAbs },
         { name: 'Capital Change (%)', stat: capitalChangePercent },
-        { name: 'Days since Portfolio Active', stat: lastModified },
+        { name: 'Days since Portfolio Active', stat: lastModified},
     ]
 
     const tableHeaders = [
-        { header: 'Stock Name', key: 'stockSymbol' },
-        { header: 'Stock Sector', key: 'stockSector' },
-        { header: 'Total Quantity', key: 'totalQuantity' },
-        { header: 'Average Price', key: 'averagePrice' },
-        { header: 'Current Price', key: 'currentValue' },
+    { header: 'Stock Name', key: 'stockSymbol' },
+    { header: 'Stock Sector', key: 'stockSector' },
+    { header: 'Total Quantity', key: 'totalQuantity' },
+    { header: 'Average Price', key: 'averagePrice' },
+    { header: 'Current Price', key: 'currentValue' },
     ];
 
 
@@ -242,17 +238,17 @@ function Portfolio() {
             <div>
                 <div className="lg:flex lg:items-center lg:justify-between my-6 px-6">
                     <h3 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-                        {/* {samplePortfolioData.name} */}
+                        {samplePortfolioData.name}
                     </h3>
                     <div className="min-w-0 flex-1">
                         <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6 mx-2">
                             <div className="mt-2 flex items-center text-sm text-gray-500">
                                 <BriefcaseIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                                {/* {samplePortfolioData.strategy} */}
+                                {samplePortfolioData.strategy}
                             </div>
                             <div className="mt-2 flex items-center text-sm text-gray-500">
                                 <CurrencyDollarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                                {/* {samplePortfolioData.capital} */}
+                                {samplePortfolioData.capital}
                             </div>
                             <div className="mt-2 flex items-center text-sm text-gray-500">
                                 <CalendarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
@@ -292,8 +288,8 @@ function Portfolio() {
             </div>
             <h4 className='font-semibold'>Portfolio Performance</h4>
             <div className="flex my-6">
-                {/* <LineChartComponent data={linechartdata}></LineChartComponent> */}
-                {/* <PieChartComponent data={piechartdata}></PieChartComponent> */}
+                <LineChartComponent data={linechartdata}></LineChartComponent>
+                <PieChartComponent data={piechartdata}></PieChartComponent>
             </div>
             <div className="my-6 px-6">
                 <Table
@@ -321,20 +317,20 @@ function Portfolio() {
                                             e.preventDefault();
                                             handleModalClose();
                                         }}>
-                                            <div className="mb-3">
+                                        <div className="mb-3">
                                                 <select required value={side} onChange={(e) => setSide(e.target.value)} onMouseUp={summary}
-                                                    className="w-full appearance-none border rounded py-2 px-3 text-gsgray70 leading-tight"
-                                                    id="side">
+                                                className= "w-full appearance-none border rounded py-2 px-3 text-gsgray70 leading-tight" 
+                                                id="side">
                                                     <option value="" disabled selected className="font-bold">Side</option>
                                                     <option value="BUY">BUY</option>
                                                     <option value="SELL">SELL</option>
                                                 </select>
                                             </div>
-
+            
                                             <div className="mb-3">
                                                 <select required value={stockCode} onChange={(e) => setStockCode(e.target.value)} onMouseUp={summary}
-                                                    className="w-full appearance-none border rounded py-2 px-3 text-gsgray70 leading-tight"
-                                                    id="stock">
+                                                className= "w-full appearance-none border rounded py-2 px-3 text-gsgray70 leading-tight"
+                                                id="stock">
                                                     {/* These should be autofilled in the future, hardcoded for now */}
                                                     <option value="" disabled selected className="font-bold">Stock Code</option>
                                                     <option value="AAPL">AAPL</option>
@@ -343,11 +339,11 @@ function Portfolio() {
                                                     <option value="META">META</option>
                                                 </select>
                                             </div>
-
+            
                                             <div className="mb-3">
                                                 <input className="appearance-none border rounded w-full py-2 px-3 text-gsgray70 leading-tight"
-                                                    id="quantity"
-                                                    type="number"
+                                                    id="quantity" 
+                                                    type="number" 
                                                     placeholder="Stock Quantity"
                                                     required
                                                     value={quantity}
@@ -357,15 +353,15 @@ function Portfolio() {
                                                 >
                                                 </input>
                                             </div>
-
+            
                                             <div className="mb-3">
                                                 <input className="appearance-none border rounded w-full py-2 px-3 text-gsgray70 leading-tight"
-                                                    id="price"
-                                                    type="number"
+                                                    id="price" 
+                                                    type="number" 
                                                     placeholder="Stock Price"
                                                     required
                                                     value={price}
-                                                    onChange={(e) => setPrice(e.target.value)}
+                                                    onChange= {(e) => setPrice(e.target.value)}
                                                     onKeyUp={summary}
                                                     onMouseUp={summary}
                                                 >
@@ -374,14 +370,14 @@ function Portfolio() {
 
                                             <div className="mb-3">
                                                 <input className="appearance-none border rounded w-full py-2 px-3 text-gsgray70 leading-tight"
-                                                    id="date"
+                                                    id="date" 
                                                     type="date"
                                                     required
                                                     value={date}
                                                     onChange={(e) => setDate(e.target.value)}
                                                     onKeyUp={summary} // for typing
                                                     onMouseUp={summary} // for clicking
-                                                    max={todayString} // limit date because you cannot buy/sell in the future
+                                                    max = {todayString} // limit date because you cannot buy/sell in the future
                                                 >
                                                 </input>
                                             </div>
@@ -392,7 +388,7 @@ function Portfolio() {
                                         </div>
                                         <hr className=""></hr>
                                         <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                            <button type="button" onClick={handleModalClose} className="inline-flex w-full justify-center rounded-md bg-gsgray70 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gsgray90 sm:ml-3 sm:w-auto">Cancel</button>
+                                            <button type="button" onClick= {handleModalClose} className="inline-flex w-full justify-center rounded-md bg-gsgray70 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gsgray90 sm:ml-3 sm:w-auto">Cancel</button>
 
                                             <button type="submit" onClick={handleButtons} className="mt-3 inline-flex w-full justify-center rounded-md bg-gsgreen50 px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gsgreen60 sm:mt-0 sm:w-auto">Add Position</button>
                                         </div>
