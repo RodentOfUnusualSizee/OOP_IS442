f267e8a369f76b38e2b06463847ed024106099ae
carleb <calebcheongdnt@gmail.com>
2023-10-27 15:54:11 +0800

    Update EmailServiceController.java

--- backend-app/src/main/java/com/app/User/EmailValidationComponent/EmailServiceController.java
+++ backend-app/src/main/java/com/app/User/EmailValidationComponent/EmailServiceController.java
@@ -1,22 +1,32 @@
 package com.app.User.EmailValidationComponent;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Base64;
-import com.app.User.User;
 
 @Controller
-@RequestMapping("/validate")
 public class EmailServiceController {
-
     @Autowired
     private EmailService emailService;
 
-    @GetMapping
+    @GetMapping("/sendEmail")
+    public ResponseEntity<String> sendEmail(@RequestParam String toEmail, @RequestParam String subject,
+            @RequestParam String message) {
+
+        try {
+            emailService.sendEmail(toEmail, subject, message);
+            return ResponseEntity.ok("Email sent");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Failed to send email error:" + e.getMessage());
+        }
+
+    }
+
+    @GetMapping("/validate")
     public String validateEmail(@RequestParam String token, Model model) {
         // Decode the token (Base64) to get the original email
         byte[] decodedBytes = Base64.getDecoder().decode(token);
