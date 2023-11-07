8f86f24407fef32ad973c124460788dd7fd47231
yyyanwee <89122919+yyyanwee@users.noreply.github.com>
2023-11-07 12:22:40 +0800

    format home

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -7,6 +7,8 @@ import '../styles/home.css';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
+import { EyeIcon } from '@heroicons/react/20/solid';
+import { EyeSlashIcon } from '@heroicons/react/24/solid';
 
 function Home() {
 
@@ -42,6 +44,7 @@ function Home() {
         } catch (error : unknown) {
             if (error instanceof Error) {
                 console.error("An error occurred during login", error);
+                alert(error.cause);
                 setError(error.message);
             } else {
                 console.error("An error occurred during login", error);
@@ -50,14 +53,17 @@ function Home() {
         }
     }
 
+    const [showPassword, setShowPassword] = React.useState<boolean>(false);
+
     return (
         <div>
             <Header management={false} userType="admin" login={false}></Header>
             <div className="container mx-auto max-w-screen-xl h-screen rounded-l p-8 grid place-items-center">
+                {/* Login Modal */}
                 {loginClicked ? (
                     <CSSTransition
                         in={loginClicked}
-                        timeout={300}
+                        timeout={300}   
                         classNames="fade"
                         unmountOnExit
                         onExit={() => setLoginClicked(false)}
@@ -69,7 +75,7 @@ function Home() {
                             </p>
                             <form className="bg-white shadow-md rounded p-8 m-10">
                                 <div className="mb-4">
-                                    <input className="shadow appearance-none border rounded w-full py-2 px-3 my-2 text-gsgray70 leading-tight"
+                                    <input className="appearance-none block w-full bg-gswhite text-gsgrey70 border border-gsgray40 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gsgray-70"
                                         id="email"
                                         type="text"
                                         placeholder="Email"
@@ -79,21 +85,27 @@ function Home() {
                                     >
                                     </input>
                                 </div>
-                                <div className="mb-4">
-                                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gsgray70 leading-tight"
+                                <div className="relative mt-2 rounded-md shadow-sm">
+                                    <input className="sappearance-none block w-full bg-gswhite text-gsgrey70 border border-gsgray40 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gsgray-70"
                                         id="password"
-                                        type="password"
+                                        type= {showPassword ? "text" : "password"}
                                         placeholder="Password"
                                         value={password}
                                         onChange={(e) => setPassword(e.target.value)}
                                         required>
                                     </input>
+
+                                    <div className="absolute inset-y-0 right-0 flex items-center pr-3">
+                                        {showPassword ?
+                                        (<EyeIcon className="h-5 w-5 text-gsgray60" aria-hidden="true" onClick={() => setShowPassword(prevState => !prevState)}/>):
+                                        (<EyeSlashIcon className="h-5 w-5 text-gsgray60" aria-hidden="true" onClick={() => setShowPassword(prevState => !prevState)}/>)} 
+                                    </div>
                                 </div>
-                                <div id="errorMessage" className="text-gsred60 text-sm font-light w-30 pb-2 px-2">
+                                <div id="errorMessage" className="text-gsred60 text-sm font-medium w-full pb-4 px-2 place-items-center">
                                     {error}
                                 </div>
                                 <div id="loginButtons">
-                                    <button className="bg-gsblue60 hover:bg-gsblue70 text-white font-light w-30 py-2 px-2 rounded-sm mx-2" type="submit" onClick={submitLogin} name="Login">
+                                    <button className="bg-gsblue60 hover:bg-gsblue70 text-gswhite font-medium w-28 py-2 px-2 rounded-sm mx-2" type="submit" onClick={submitLogin} name="Login">
                                         Login
                                     </button>
 
@@ -116,13 +128,15 @@ function Home() {
                     </CSSTransition>
                 ) :
                     <div>
+                        {/* Initial Landing Page */}
                         <img src="/images/gs-blue.png" className="w-20 mb-6 mx-auto"></img>
                         <p className="text-4xl text-gray-700 font-bold mb-5">
                             Portfolio Performance Analyser App
                         </p>
+                        {/* Login and Register buttons - out of modal */}
                         <p className="container mx-auto text-sm">
-                            <button className=" bg-gsblue60 hover:bg-gsblue70 focus:outline-none focus:ring-2 focus:ring-black w-20 mx-2 px-2 py-2 text-white rounded-sm font-light" onClick={() => setLoginClicked(true)} >Login</button>
-                            <button className=" bg-gsblue60 hover:bg-gsblue70 focus:outline-none focus:ring-2 focus:ring-black w-20 mx-2 px-2 py-2 text-white rounded-sm font-light"><a href="registration">Register</a></button>
+                            <button className=" bg-gsblue60 hover:bg-gsblue70 focus:outline-none focus:ring-2 focus:ring-black w-20 mx-2 px-2 py-2 text-gswhite rounded-sm font-light" onClick={() => setLoginClicked(true)} >Login</button>
+                            <button className=" bg-gsblue60 hover:bg-gsblue70 focus:outline-none focus:ring-2 focus:ring-black w-20 mx-2 px-2 py-2 text-gswhite rounded-sm font-light"><a href="registration">Register</a></button>
                         </p>
                     </div>
                 }
